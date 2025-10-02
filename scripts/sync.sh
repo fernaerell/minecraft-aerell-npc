3f9aaa34e55f6f23aef2939a9e5277988345bafb
fernaerell <fernaerell2020@gmail.com>
2025-10-02 21:52:33 +0700

    chore(script): add sync script for addon

---
+++ scripts/sync.sh
@@ -0,0 +1,25 @@
+NAME="aerell-partner"
+
+BP_DIR_PATH="addon/behavior"
+RP_DIR_PATH="addon/resource"
+
+DEV_BP_DIR_PATH="C:/Users/$(whoami)/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_behavior_packs"
+DEV_RP_DIR_PATH="C:/Users/$(whoami)/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_resource_packs"
+
+BP_DIR_PATH_DEST="$DEV_BP_DIR_PATH/$NAME"
+RP_DIR_PATH_DEST="$DEV_RP_DIR_PATH/$NAME"
+
+if [[ ! -d $DEV_BP_DIR_PATH || ! -d $DEV_RP_DIR_PATH ]]; then
+    echo "Development behavior or resource pack folder not exist."
+fi
+
+if [ -d $BP_DIR_PATH_DEST ]; then
+    rm -r $BP_DIR_PATH_DEST
+fi
+
+if [ -d $RP_DIR_PATH_DEST ]; then
+    rm -r $RP_DIR_PATH_DEST
+fi
+
+cp -r $BP_DIR_PATH $BP_DIR_PATH_DEST
+cp -r $RP_DIR_PATH $RP_DIR_PATH_DEST
