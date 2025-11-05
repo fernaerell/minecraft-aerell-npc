7fad0a66968753b91e299f8897d37a0b9f2b37ee
fernaerell <fernaerell2020@gmail.com>
2025-11-06 00:55:55 +0700

    chore: Update sync script with the new location folder of development rp and bp

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,24 +2,23 @@ NAME="aerell-npc"
 
 BP_DIR_PATH="addon/behavior"
 RP_DIR_PATH="addon/resource"
-
-DEV_BP_DIR_PATH="C:/Users/$(whoami)/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_behavior_packs"
-DEV_RP_DIR_PATH="C:/Users/$(whoami)/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_resource_packs"
+DEV_BP_DIR_PATH="C:/Users/$(whoami)/AppData/Roaming/Minecraft Bedrock/Users/Shared/games/com.mojang/development_behavior_packs"
+DEV_RP_DIR_PATH="C:/Users/$(whoami)/AppData/Roaming/Minecraft Bedrock/Users/Shared/games/com.mojang/development_resource_packs"
 
 BP_DIR_PATH_DEST="$DEV_BP_DIR_PATH/$NAME"
 RP_DIR_PATH_DEST="$DEV_RP_DIR_PATH/$NAME"
 
-if [[ ! -d $DEV_BP_DIR_PATH || ! -d $DEV_RP_DIR_PATH ]]; then
+if [[ ! -d "$DEV_BP_DIR_PATH" || ! -d "$DEV_RP_DIR_PATH" ]]; then
     echo "Development behavior or resource pack folder not exist."
 fi
 
-if [ -d $BP_DIR_PATH_DEST ]; then
-    rm -r $BP_DIR_PATH_DEST
+if [ -d "$BP_DIR_PATH_DEST" ]; then
+    rm -r "$BP_DIR_PATH_DEST"
 fi
 
-if [ -d $RP_DIR_PATH_DEST ]; then
-    rm -r $RP_DIR_PATH_DEST
+if [ -d "$RP_DIR_PATH_DEST" ]; then
+    rm -r "$RP_DIR_PATH_DEST"
 fi
 
-cp -r $BP_DIR_PATH $BP_DIR_PATH_DEST
-cp -r $RP_DIR_PATH $RP_DIR_PATH_DEST
+cp -r $BP_DIR_PATH "$BP_DIR_PATH_DEST"
+cp -r $RP_DIR_PATH "$RP_DIR_PATH_DEST"
